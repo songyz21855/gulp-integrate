6e04809d8195c6fd89c6fe67084548f6ee5667c4
zhousony <2877293570@qq.com>
2016-12-17 15:48:10 +0800

    sass编译

--- gulpfile.js
+++ gulpfile.js
@@ -12,10 +12,11 @@ var gulp         = require('gulp'),
     uglify       = require('gulp-uglify'),         // js文件压缩
     cache        = require('gulp-cache'),          // 图片缓存，只有图片替换了才压缩
     less         = require('gulp-less'),           // less文件编译
+    sass         = require('gulp-sass'),           // sass文件编译
     watch        = require('gulp-watch');          // 文件监听
 
 var gulpSrc  = './src',    // 源码目录
-    gulpDist = './dist';  // 打包目录
+    gulpDist = './dist';   // 打包目录
 
 // gulp 源文件与目标文件基本配置
 var gulpPath = {
@@ -24,7 +25,10 @@ var gulpPath = {
             gulpSrc + '/less/**/*.less',
             '!' + gulpSrc + '/less/*_un.less'          // 不编译
         ],
-        sass  : [],
+        sass  : [
+            gulpPath + '/sass/**/*.{scss,sass}',
+            '!' + gulpPath + '/sass/**/*_un.{scss,sass}'     // 不编译
+        ],
         images: [
             gulpSrc + '/images/**/**',
             '!' + gulpSrc + '/images/**/*.psd'         // 不编译
@@ -51,7 +55,21 @@ gulp.task('less', function () {
         .pipe(minifycss())
         .pipe(gulp.dest(gulpPath.dist.css))
         .pipe(notify({message: 'less 文件有更改!'}));
-    ;
+});
+
+// 编译sass,scss文件
+gulp.task('sass',function () {
+    return gulp.src(gulpPath.src.sass)
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(autoprefixer({
+            browses: ['last 2 versions', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'],
+            cascade: false
+        }))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(minifycss())
+        .pipe(gulp.dest(gulpPath.dist.css))
+        .pipe(notify({message: 'sass 文件有更改!'}));
 })
 
 // 图片压缩
@@ -82,8 +100,11 @@ gulp.task('watch', function () {
     // 监听所有less文件
     gulp.watch(gulpPath.src.less[0], ['less']);
 
+    // 监听所有的sass文件
+    gulp.watch(gulpPath.src.sass[0], ['sass']);
+
     // 监听所有图片文件
-    gulp.watch(gulpPath.src.images, ['images']);
+    gulp.watch(gulpPath.src.images[0], ['images']);
 
     // 监听所有脚本文件
     gulp.watch(gulpPath.src.js, ['script']);
@@ -91,4 +112,4 @@ gulp.task('watch', function () {
 });
 
 // 默认任务
-gulp.task('default', ['less', 'images', 'script']);
+gulp.task('default', ['watch','less','sass','images', 'script']);
